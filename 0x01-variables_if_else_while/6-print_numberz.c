a0a7e4178887466ef3493631a4fd1df4b91b564c
3ple-C <chukwumachimaroke12@gmail.com>
2022-12-13 00:21:33 -0800

    ;Number_Z

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	short int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar((a % 10) + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
