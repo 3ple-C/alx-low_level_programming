4801fe7bf56d36b487f6aa0262dc2f3a78ab0ef0
3ple-C <chukwumachimaroke12@gmail.com>
2022-12-13 08:06:21 -0800

    Alpha Function prototype

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** void - No return value
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+void print_alphabet(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	
+	_putchar('\n');
+	return;
+}
