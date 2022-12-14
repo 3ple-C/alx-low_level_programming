e68a4c9d311bc10e2a56ebda811f03da5de19aed
3ple-C <chukwumachimaroke12@gmail.com>
2022-12-14 12:04:37 -0800

    Number sign task

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -7,7 +7,13 @@
  */
 int main(void)
 {
-	putchar('+');
+	int r;
+
+	r = print_sign(98);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
 
 	return (0);
 }
