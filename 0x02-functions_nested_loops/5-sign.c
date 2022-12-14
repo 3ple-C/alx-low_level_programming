a43b4cd43da29cb542cb2805a56ed95e0125b3eb
3ple-C <chukwumachimaroke12@gmail.com>
2022-12-14 11:46:35 -0800

    No. sign task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints sign of a number
+ * @n: Number with sign
+ *
+ * Return: 0 If any other condition isnt met
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar(0);
+		return (0);
+	}
+}
