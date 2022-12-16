8d2ea5ea4a1128aac43fe73e039ec848c149c805
3ple-C <chukwumachimaroke12@gmail.com>
2022-12-16 08:42:12 -0800

    Arithmetic function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiples two integers
+ * @a: first operator
+ * @b: second operator
+ *
+ * Return: multiplication of a & b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
