1be194148c2789b08351d59f59a76b7c45c3ec91
3ple-C <chukwumachimaroke12@gmail.com>
2023-01-09 06:02:00 -0800

    Prints every arguments passed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - Entry point, prints each argument passed
+* @argc: number of arguments passed
+* @argv: string array of arguments
+*
+* Return: 0, Success
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
