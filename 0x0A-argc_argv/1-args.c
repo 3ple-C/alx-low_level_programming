a1c5b429089e2d30be4b44bc4a0501d7fc15859d
3ple-C <chukwumachimaroke12@gmail.com>
2023-01-09 05:18:57 -0800

    Prints the number of arguments passed from command line

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - Entry point, prints number of arguments passed
+* @argc: number of argument passed
+* @argv: array of arguments
+*
+* Return: 0, Success
+*/
+int main(int argc, char *argv[])
+{
+	int i, sum;
+	for (i = 0; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", argc - 1);
+	
+	return (0);
+}
