86020c327adea7b332f9c5c6890c65c48386ef87
3ple-C <triplec871@gmail.com>
2023-07-14 09:27:09 -0700

    prints all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - Prints a hexadecimal string
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
